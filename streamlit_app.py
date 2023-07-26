e9cee7f9c8e62efcdd24b266b410a06c707ee5bc
111Aa111222 <140500061+111Aa111222@users.noreply.github.com>
2023-07-26 15:11:57 +0800

    Update streamlit_app.py
    create another header for a special menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
 
 st.text('🥣Omega 3 & Blueberry Oatmeal \n🥗Kale, Spinach & Rocket Smoothie \n🐔Hard-Boiled Free-Range Egg\n🥑🍞Avocado Toast')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
