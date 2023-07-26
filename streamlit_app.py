e52789c4b15aa6c0ccd7fa675218ff4f39d2b6bf
111Aa111222 <140500061+111Aa111222@users.noreply.github.com>
2023-07-26 15:08:19 +0800

    Update streamlit_app.py
    add enter on text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
 
-st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard-Boiled Free-Range Egg')
