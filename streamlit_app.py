46121a861db734f6d746fcff2d65e1dade3aa1c2
111Aa111222 <140500061+111Aa111222@users.noreply.github.com>
2023-07-26 15:09:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
 
-st.text('Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard-Boiled Free-Range Egg')
+st.text('Omega 3 & Blueberry Oatmeal \nKale, Spinach & Rocket Smoothie \nHard-Boiled Free-Range Egg')
