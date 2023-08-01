d8c90b52f823e769aa333eba4f0651bf1465a3ad
111Aa111222 <140500061+111Aa111222@users.noreply.github.com>
2023-08-01 17:33:27 +0800

    Update streamlit_app.py
    separate the base url from the fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ st.dataframe(fruit_to_show)
 st.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+
 
 #take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
