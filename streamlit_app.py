d3fc40ae3a8962f141814c8efbe983f824997e28
111Aa111222 <140500061+111Aa111222@users.noreply.github.com>
2023-08-11 14:26:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,10 @@ import snowflake.connector
 
 st.text("Hello from Snowflake:")
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+
+streamlit.text(my_data_row)
+
